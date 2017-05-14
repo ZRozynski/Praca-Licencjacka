38d668f618471a81b0abc083654fbd2989738a59
ZRozynski <zachary.p.rozynski@gmail.com>
2017-05-14 15:38:51 +0200

    Klasa Graf oraz wszystkie składowe.

---
+++ Praca licencjacka/Graph.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praca_licencjacka
+{
+    class Graph
+    {
+        private static Graph _instance;
+        private List<Vertex> _graph;
+
+        public void AddNewVertex(Vertex newVertex)
+        {
+            this._graph.Add(newVertex);
+        }
+
+        public void RemoveVertex(Vertex toRemove)
+        {
+            toRemove.RemoveAllNeighbours();
+            this._graph.Remove(toRemove);       
+        }
+    
+        public Vertex GetVertexColliding(Point coordinates)
+        {
+            foreach(Vertex currentVertex in this._graph)
+            {
+                if (currentVertex.IsColliding(coordinates))
+                    return currentVertex;
+            }
+            return null;
+        }
+
+        private Graph()
+        {
+            this._graph = new List<Vertex>();
+        }
+
+        public static Graph GetInstance()
+        {
+            if(_instance.Equals(null))
+            {
+                _instance = new Graph();
+                return _instance;
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Praca licencjacka/Vertex.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praca_licencjacka
+{
+    class Vertex
+    {
+        private int _xPos;
+        private int _yPos;
+        private List<Edge> _neighbours;
+        public int vertexSizeInPixels = 25;
+
+        public Vertex(int xPos, int yPos)
+        {
+            this._xPos = xPos;
+            this._yPos = yPos;
+            this._neighbours = new List<Edge>();
+        }
+        
+        public void SetPosition(int newXPos, int newYPos)
+        {
+            this._xPos = newXPos;
+            this._yPos = newYPos;
+        }
+
+        public int GetX()
+        {
+            return this._xPos;
+        }
+
+        public bool IsColliding(Point coordinates)
+        {
+            return (coordinates.X <= this._xPos + this.vertexSizeInPixels && coordinates.X >= this._xPos - this.vertexSizeInPixels
+                && coordinates.Y >= this._yPos - this.vertexSizeInPixels && coordinates.Y <= this._yPos + this.vertexSizeInPixels) ;
+        }
+
+        public int GetY()
+        {
+            return this._yPos;
+        }
+
+        public void AddNewNeighbour(Vertex neighbour)
+        {
+            double distanceBetween = this.CalculateDistance(neighbour);
+            this._neighbours.Add(new Edge(neighbour, distanceBetween));
+        }
+
+        public void RemoveAllNeighbours()
+        {
+            foreach(Edge currentNeighbour in this._neighbours)
+            {
+                Vertex destination = currentNeighbour.GetDestination();
+                destination.RemoveNeighbour(this);
+            }
+        }
+
+        public void RemoveNeighbour(Vertex neighbour)
+        {
+            Edge chosenNeighbour;
+            if((chosenNeighbour = this.GetEdgeByVertex(neighbour)).Equals(null)){
+                this._neighbours.Remove(chosenNeighbour);
+            }
+        }
+
+        // After changing Vertex position it is very important to change travel costs to all it's neighbours
+        private void RecalculateTravelCost()
+        {
+            //TODO
+        }
+
+        public Point GetVertexPosition()
+        {
+            return new Point(this._xPos, this._yPos);
+        }
+
+        public double CalculateDistance(Vertex destination)
+        {
+            double xDistance = Math.Pow(Math.Abs(this._xPos - destination.GetX()), 2);
+            double yDistance = Math.Pow(Math.Abs(this._yPos - destination.GetY()), 2);
+            double destinationDistance = Math.Sqrt(xDistance + yDistance);
+            return destinationDistance;
+        }
+
+        public Edge GetEdgeByVertex(Vertex neighbour)
+        {
+            foreach(Edge currentNeighbour in this._neighbours)
+            {
+                if (currentNeighbour.GetDestination().Equals(neighbour))
+                    return currentNeighbour;
+            }
+            return null;
+        }
+    }
+}
